2551a2860abf35e12511570435e3f710c3ba406c
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-10-07 21:21:00 +0300

    Fixed the path in App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import {
   BrowserRouter,
   Routes, Route,
 } from 'react-router-dom';
-import Books from './components/Books';
-import Categories from './components/Categories/Categories';
-import Nav from './components/Nav/Nav';
+import Books from './Components/Books';
+import Categories from './Components/Categories/Categories';
+import Nav from './Components/Nav/Nav';
 
 function App() {
   return (
