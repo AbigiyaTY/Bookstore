b0280acb0419ef97cac99abe46a0c6b865a5f1d0
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-10-07 20:40:54 +0300

    Fixed linter error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
 } from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories/Categories';
-import Nav from './Nav/Nav';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
